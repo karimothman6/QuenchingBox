1dfc177fd4c6a18e949505430e9051f059eac0e5
Karim Othman <104131956+karimothman6@users.noreply.github.com>
2025-02-06 12:24:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,6 @@ st.set_page_config(
 st.title("Quenching Box - Main Page")
 st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVe1HRt5eIRvbsvsnGjlKVqJTIJbLQbBWgSErE-AkE5JZeAIAjMoq87bteilcF-rLyRM8uFv4kj9Cc18a_OxnnJnxKScepazpcLnc_p3RHdKUtBxXMY74AQ31XjYDBBJzCd4aGpEeNjTeY/s640/logo-2.png")
 
-# Navigation Bar
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Optimize", "Predict"])
-
-if page == "Optimize":
-    st.switch_page("pages/optimize.py")
-elif page == "Predict":
-    st.switch_page("pages/predict.py")
-
 # Main Page Content
 st.write("Welcome to the Quenching Box Application. Select an option to proceed.")
 
