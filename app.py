5182d53b6977c0b240019a6dc3daf52c9795fb02
Karim Othman <104131956+karimothman6@users.noreply.github.com>
2025-02-06 12:34:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.set_page_config(
 st.title("Quenching Box - Main Page")
 st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVe1HRt5eIRvbsvsnGjlKVqJTIJbLQbBWgSErE-AkE5JZeAIAjMoq87bteilcF-rLyRM8uFv4kj9Cc18a_OxnnJnxKScepazpcLnc_p3RHdKUtBxXMY74AQ31XjYDBBJzCd4aGpEeNjTeY/s640/logo-2.png")
 
-# Main Page Content
 st.write("Welcome to the Quenching Box Application. Select an option to proceed.")
 
 col1, col2 = st.columns(2)
